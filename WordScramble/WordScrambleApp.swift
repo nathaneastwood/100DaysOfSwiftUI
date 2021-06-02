2a495f6f1c2111f7a35a5ed89b428298c9e45b83
nathaneastwood <nathan.eastwood@icloud.com>
2021-06-02 19:43:13 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nathan Eastwood on 02/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
